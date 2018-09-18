26ebacab45545ffd023bca4ac8ab457900dcbefe
Daniele Brugnara <brugnara.daniele@gmail.com>
2018-09-18 13:52:51 +0200

    Still building
    Loaded interpreter and TFlite

--- app/src/main/java/com/example/daniele/drawapp/MainActivity.java
+++ app/src/main/java/com/example/daniele/drawapp/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.daniele.drawapp;
 
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import org.tensorflow.lite.Interpreter;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawView drawView;
     private TextView textView;
+
+    String modelFile="model.tflite";
+    Interpreter tflite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         drawView.init(metrics);
+
+        try {
+            tflite = new Interpreter(loadModelFile(this, modelFile));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void onButtonTap(View v){
@@ -30,6 +48,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonTapTest(View v){
         textView.setText("3");
+    }
+
+
+    private MappedByteBuffer loadModelFile(Activity activity, String MODEL_FILE) throws IOException {
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(MODEL_FILE);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
     }
 
 }
