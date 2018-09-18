12cb54ad622b27f0b6ab5a1084c65bb90f81980a
Daniele Brugnara <brugnara.daniele@gmail.com>
2018-09-18 13:22:49 +0200

    Now Drawing!

--- app/src/main/java/com/example/daniele/drawapp/MainActivity.java
+++ app/src/main/java/com/example/daniele/drawapp/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.daniele.drawapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.DisplayMetrics;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawView drawView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawView = findViewById(R.id.drawView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        drawView.init(metrics);
     }
+
+
+
+
 }
